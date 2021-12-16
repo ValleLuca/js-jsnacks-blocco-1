692a24058ad5ee3a6d3c3c4bf5b7441e04a6b65e
ValleLuca <valle.lucaAb@outlook.it>
2021-12-16 15:41:28 +0100

    jsnack1 finito

--- jsnack1/myscript.js
+++ jsnack1/myscript.js
@@ -1,8 +1,17 @@
 const vuotoArr = [];
 let somma = 0;
+let i = 0;
+const risultatoFinale = document.getElementById("risultato");
 
-while(somma > 50){
-    const number = parseInt(prompt("inserire numero"));
-    somma += number;
-    console.log("somma");
+
+while(somma < 50){
+    const n = parseInt(prompt("inserire numero"));
+    vuotoArr.push(n);
+    somma += vuotoArr[i];
+    i++;
+    console.log(somma);
 }
+
+
+
+
