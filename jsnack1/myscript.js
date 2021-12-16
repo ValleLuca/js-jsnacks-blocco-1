c1a579e4b45200dc31adbc7155c7bbbe17f159b7
ValleLuca <valle.lucaAb@outlook.it>
2021-12-16 15:46:33 +0100

    Aggiunti commennti in jsnack1 e consegne

--- jsnack1/myscript.js
+++ jsnack1/myscript.js
@@ -1,15 +1,16 @@
-const vuotoArr = [];
-let somma = 0;
-let i = 0;
-const risultatoFinale = document.getElementById("risultato");
+//Consegna
+/*Crea un array vuoto e chiedi all’utente un numero da inserire nell’array. Continua a chiedere i numeri all’utente e a inserirli nell’array fino a quando la somma degli elementi è minore di 50.*/
 
+const vuotoArr = []; //dichiarazione array
+let somma = 0; // dichiarazione somma
+let i = 0; // dichiarazione "i" per contatore
 
-while(somma < 50){
-    const n = parseInt(prompt("inserire numero"));
-    vuotoArr.push(n);
-    somma += vuotoArr[i];
-    i++;
-    console.log(somma);
+while(somma < 50){ //while per controllo max 50 numero
+    const n = parseInt(prompt("inserire numero")); // input operatore via prompt
+    vuotoArr.push(n); // aggiunta nell'array attraverso il push
+    somma += vuotoArr[i]; //somma dell'input con il valore nell'array
+    i++; //aumento contatore
+    console.log(somma); //stampo valore di somma ogni volta che si ripete il ciclo
 }
 
 
---
+++ jsnack3/myscript.js
@@ -0,0 +1,3 @@
+//consegna
+/*un div avrà il testo colorato di rosso mentre l’altro di verde.
+Partendo da un array di numeri, stampiamo nell’id rosso i numeri dispari e in verde i numeri pari.*/
