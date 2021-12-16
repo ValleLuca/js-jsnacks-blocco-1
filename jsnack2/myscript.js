e65ad5cea00af5801862672b1719a36a863b73a8
ValleLuca <valle.lucaAb@outlook.it>
2021-12-16 18:11:36 +0100

    jsnack2 concluso

--- jsnack2/myscript.js
+++ jsnack2/myscript.js
@@ -3,5 +3,18 @@
 Genera N array, ognuno formato da 10 numeri casuali tra 1 e 100.
 Ogni volta che ne crei uno, stampalo a schermo.*/
 
-let datoN = parseInt(prompt("Inserire dato"));
+let datoN = parseInt(prompt("Inserire dato")); //dato inserito dall'utente
 
+
+
+for(let i = 0; i < datoN; i++){ //ciclo per più array
+
+    let newArr = []; //creo una nuova array
+
+    for(let i2 = 0; i2 < 10; i2++){ //per generare numeri random e aggiungerli
+        let x = Math.floor((Math.random() * 100) + 1);
+        newArr.push(x);
+    }
+    console.log(newArr);
+    
+}
